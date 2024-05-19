f7aea5603822c70624337d852673cdc52cdb8408
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-19 08:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let input = document.getElementById("newTodoInput");
+let list = document.getElementById("todoList");
+let btn = document.getElementById("addTodoBtn");
 
+btn.onclick = (()=>{
+    if(input.value.length!=0)
+        list.innerHTML+=`<li>${input.value}</li>`;
+})
+
+// btn.disabled = false;
+// console.log(btn);
